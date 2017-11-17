ea2b4c22338d5b644c87981e13e709c31ebd59cc
zww <zwei_ios@163.com>
2017-11-17 11:53:35 +0800

    all

---
+++ ZSHApp/Modules/Buy(尊购)/Controller/私人订制/Model/TailorDetailModel.m
@@ -0,0 +1,13 @@
+//
+//  TailorDetailModel.m
+//  ZSHApp
+//
+//  Created by Apple on 2017/11/9.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import "TailorDetailModel.h"
+
+@implementation TailorDetailModel
+
+@end
